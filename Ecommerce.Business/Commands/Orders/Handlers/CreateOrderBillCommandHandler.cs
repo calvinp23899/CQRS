dfa604e2bf64a1f5bd643b54a481da1e6b837e7a
calvin <calvinp2381999@gmail.com>
2025-01-07 22:50:01 +0700

    send email success for creating order

--- Ecommerce.Business/Commands/Orders/Handlers/CreateOrderBillCommandHandler.cs
+++ Ecommerce.Business/Commands/Orders/Handlers/CreateOrderBillCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Ecommerce.Business.Commands.Users;
+using Ecommerce.Core.Constants;
 using Ecommerce.Core.DTOs.User;
 using Ecommerce.Core.Interfaces;
 using Ecommerce.Core.Models;
@@ -16,36 +17,40 @@ namespace Ecommerce.Business.Commands.Orders.Handlers
         private IRepository<Order> _repository;
         private IRepository<OrderDetail> _repositoryOrderDetail;
         private IRepository<Core.Models.Product> _repositoryProduct;
+        private IRepository<Core.Models.User> _repositoryUser;
         private IUserService _userService;
+        private IEmailService _emailService;
 
-        public CreateOrderBillCommandHandler(IRepository<Order> repository, IUserService userService, IRepository<OrderDetail> repositoryOrderDetail, IRepository<Product> repositoryProduct)
+        public CreateOrderBillCommandHandler(
+            IRepository<Order> repository, 
+            IUserService userService, 
+            IRepository<OrderDetail> repositoryOrderDetail, 
+            IRepository<Product> repositoryProduct,
+            IEmailService emailService,
+            IRepository<User> repositoryUser)
         {
             _repository = repository;
             _userService = userService;
             _repositoryOrderDetail = repositoryOrderDetail;
             _repositoryProduct = repositoryProduct;
+            _emailService = emailService;
+            _repositoryUser = repositoryUser;
         }
 
         public async Task<bool> Handle(CreateOrderBillCommand request, CancellationToken cancellationToken)
         {
-            try
+            var userId = _userService.GetUserId();
+            var listOrderDetail = await NewListOrderDetail(request);
+            decimal total = listOrderDetail.Sum(x => x.UnitPrice * x.Quantity).Value;
+            var order = new Order
             {
-                var userId = _userService.GetUserId();
-                var listOrderDetail = await NewListOrderDetail(request);
-                decimal total = listOrderDetail.Sum(x => x.UnitPrice * x.Quantity).Value;
-                var order = new Order
-                {
-                    UserId = userId,
-                    OrderDetails = listOrderDetail,
-                    TotalPrice = total
-                };
-                await _repository.CreateAsync(order);
-                return true;
-            }
-            catch (Exception ex)
-            {
-                throw new ArgumentException(ex.InnerException.Message);
-            }
+                UserId = userId,
+                OrderDetails = listOrderDetail,
+                TotalPrice = total
+            };
+            await _repository.CreateAsync(order);
+            await SendEmail(userId);
+            return true;
         }
 
         private async Task<List<OrderDetail>> NewListOrderDetail(CreateOrderBillCommand request)
@@ -67,6 +72,20 @@ namespace Ecommerce.Business.Commands.Orders.Handlers
                 await _repositoryOrderDetail.CreateAsync(orderDetails);
             }
             return result;
+        }
+
+        private async Task<bool> SendEmail(int userId)
+        {
+            var user = await _repositoryUser.GetByIdAsync(userId);
+            var listEmails = new List<string>();
+            listEmails.Add(user.Email);
+            var body = string.Format(EmailBodyConstant.OrderSuccessMessage, new Guid());
+            await _emailService.SendEmailAsync(
+                listEmails,
+                "Your Order is placed successfully",
+                body,
+                null);
+            return true;
         }
     }
 }
